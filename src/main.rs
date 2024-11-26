f3e3de67a76875f5a620049b351d34c3b49f9d20
Gaurav Sablok <gauravcodepro@gmail.com>
2024-11-26 22:46:53 +0100

    applicativeystem
    started coding this application

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+/*
+
+Author Gaurav Sablok
+Universitat Potsdam
+Date: 2024-11-27
+
+A rustlang to make the deep learning alignment classification with the defined weights for inputing into the
+neural network. The classification of the weights can be done on the basis of the filtered alignment, tagged 
+alignment and the generative alignment. 
+
+*/
+
+fn main() {
+    
+}
